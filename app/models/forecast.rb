c311a253982c7efc626e9670bd67e7e21c2cc8ad
Max Mitrani <mjmitrani168@gmail.com>
2020-08-02 17:33:56 -0600

    add background spec

--- app/models/forecast.rb
+++ app/models/forecast.rb
@@ -1,7 +1,7 @@
 class Forecast
   attr_reader :id, :current_weather, :hourly_weather, :daily_weather
   def initialize(data)
-    @id = "1"
+    @id = nil
     @current_weather = data[:current_data]
     @hourly_weather = data[:hourly_data]
     @daily_weather = data[:daily_data]
---
+++ spec/requests/background_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Background API" do
+  it "can get forecast" do
+
+    get '/api/v1/backgrounds?location=denver,co'
+
+    expect(response).to be_successful
+    background = JSON.parse(response.body)
+    expect(weather["data"]["attributes"].count).to eq(2)
+
+  end
+end
