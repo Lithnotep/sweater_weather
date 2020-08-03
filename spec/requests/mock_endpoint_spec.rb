55b8e4b6125fb10d9e6f8289a0158d247865aca2
Max Mitrani <mjmitrani168@gmail.com>
2020-08-03 09:06:07 -0600

    add trails endpoint spec

--- spec/requests/mock_endpoint_spec.rb
+++ spec/requests/mock_endpoint_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 describe " Trails API" do
   it "can get trails", :vcr do
 
-    get /api/v1/trails?location=denver,co
+    get "/api/v1/trails?location=denver,co"
 
     expect(response).to be_successful
     mock = JSON.parse(response.body)
