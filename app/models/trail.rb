58698d83780e1b62e4cd3e20a0a89aee27a5d370
Max Mitrani <mjmitrani168@gmail.com>
2020-08-03 10:56:31 -0600

    add passing trail model

--- app/models/trail.rb
+++ app/models/trail.rb
@@ -1,8 +1,10 @@
 class Trail
   attr_reader :id,
-  def initialize(data)
+  def initialize(location, forecast, trail)
     @id = nil
-
+    @location = location
+    @forecast = forecast
+    @trail = trail
   end
 
 end
