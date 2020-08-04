9f8fc8d1277f98be6adffbc27870cacfc076881a
Max Mitrani <mjmitrani168@gmail.com>
2020-08-04 17:14:03 -0600

    add session sad path

--- spec/requests/sessions_endpoint_spec.rb
+++ spec/requests/sessions_endpoint_spec.rb
@@ -13,4 +13,14 @@ describe "session API" do
     expect(user["data"]["attributes"]).to have_key("api_key")
 
   end
+  it "can create session" do
+    user = create(:user, email: "whatever@example.com", password: "password")
+        post "/api/v1/sessions", :params => {
+        "email": "whatever@example.com",
+        "password": "wrong"
+      }
+
+    status = response.status
+    expect(status).to eq(401)
+  end
 end
