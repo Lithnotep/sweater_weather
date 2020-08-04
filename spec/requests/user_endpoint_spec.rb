02a538d7003c0acb447f6a27bf473fa587289e5a
Max Mitrani <mjmitrani168@gmail.com>
2020-08-03 18:50:44 -0600

    add sad path testing for user

--- spec/requests/user_endpoint_spec.rb
+++ spec/requests/user_endpoint_spec.rb
@@ -15,5 +15,29 @@ describe "User API" do
     user = JSON.parse(response.body)
     expect(user["data"]["attributes"]["email"]).to eq("whatever@example.com")
   end
+    it "can create User", :vcr do
+
+      post "/api/v1/users", :params => {
+
+        "email": "whatever@example.com",
+        "password": "password",
+        "password_confirmation": "password"
+
+      }
+
+      expect(response).to be_successful
+      user = JSON.parse(response.body)
+      expect(user["data"]["attributes"]["email"]).to eq("whatever@example.com")
+
+      post "/api/v1/users", :params => {
+
+        "email": "whatever@example.com",
+        "password": "password",
+        "password_confirmation": "password"
+      }
+      user = JSON.parse(response.body)
+      expect(user["email"].first).to eq("has already been taken")
+
+    end
 
 end
